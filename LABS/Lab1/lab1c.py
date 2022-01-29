0951203c27728ba589d740431904358f966d1d7f
lyakosh <leila.bekturgan@gmail.com>
2022-01-29 19:35:00 +0600

    Update lab1c.py

--- LABS/Lab1/lab1c.py
+++ LABS/Lab1/lab1c.py
@@ -1,1 +1,10 @@
-print(input().lower())
+def to_lower(s):
+    a=''
+    for c in s:
+        if ord('A')<=ord(c)<=ord('Z'):
+            a+=chr(ord(c)+32)
+        else:
+            a+=c
+    return a
+s=input()
+print(to_lower(s))
