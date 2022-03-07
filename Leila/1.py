86b4d1970fba4edc42ab0e724187aae268985f6d
Leila Bekturgan <leila.bekturgan@gmail.com>
2022-03-07 19:56:01 +0600

    Update 1.py

--- Leila/1.py
+++ Leila/1.py
@@ -1,15 +1,1 @@
-
-shelf = []
-taken = []
-
-n = int(input())
-for _ in range(n):
-    z = [i for i in input().split()]
-    if z[0] == '1':
-        shelf.append(z[1])
-    elif z[0] == '2':
-        taken.append(shelf[0])
-        del shelf[0] 
-
-for i in taken:
-    print(i,end=' ')
+print(eval(input().replace(' ', '*')))
