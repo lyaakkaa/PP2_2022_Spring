e45f711fcca355899ca70fe0eec72f30769facd8
Leila Bekturgan <leila.bekturgan@gmail.com>
2022-04-28 00:41:05 +0600

    Create insert_byterm.py

---
+++ LABS/lab10/PhoneBook/insert_byterm.py
@@ -0,0 +1,31 @@
+import psycopg2,csv
+from config import params
+db = psycopg2.connect(**params)
+
+
+cursor = db.cursor()
+insert = """
+    INSERT INTO PhoneBook VALUES(%s, %s, %s) returning *;
+"""
+
+
+with open("phone.csv", "r") as f:
+        reader = csv.reader(f, delimiter=",")
+        for row in reader:
+            pass
+            #cursor.execute(insert, row)
+            #result.append(cursor.fetchone())
+try:
+    name = input("Enter the name: ")
+    phoneNumber = input("Enter the number: ")
+    city = input("Enter the city: ")
+    cursor.execute(insert, (name,phoneNumber,city))
+    print("Successfully added")
+except:
+    print("This name has already been used, please write another")
+
+
+
+cursor.close()
+db.commit()
+db.close()
