6fe40fc7057bea13acc843903f82459c68c91c38
Leila Bekturgan <leila.bekturgan@gmail.com>
2022-05-05 00:48:32 +0600

    Create delete.py

---
+++ LABS/lab11/PhoneBook/delete.py
@@ -0,0 +1,17 @@
+import psycopg2
+from config import params
+db = psycopg2.connect(**params)
+
+cursor = db.cursor()
+
+name = input()
+
+sql = """
+    DELETE FROM PhoneBook WHERE name = %s;
+"""
+cursor.execute(sql, (name,))
+
+cursor.close()
+db.commit()
+db.close()
+
